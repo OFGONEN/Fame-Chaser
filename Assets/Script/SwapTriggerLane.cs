8df057bc2dc14d00e9f3a8cb607e21c656cbbe27
ofgonen <ofgonen@gmail.com>
2021-12-25 14:35:33 +0300

    SwapTriggerLane: Gizmos for helping level design

--- Assets/Script/SwapTriggerLane.cs
+++ Assets/Script/SwapTriggerLane.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 using NaughtyAttributes;
+using UnityEditor;
 
 public abstract class SwapTriggerLane : MonoBehaviour
 {
 #region Fields
-    [ BoxGroup( "Setup" ), SerializeField ] private TriggerListener_Enter swap_trigger;
-    [ BoxGroup( "Setup" ), SerializeField ] private Transform swap_point_int;
-    [ BoxGroup( "Setup" ), SerializeField ] private Transform swap_point_out;
+    [ BoxGroup( "Setup" ), SerializeField ] protected TriggerListener_Enter swap_trigger;
+    [ BoxGroup( "Setup" ), SerializeField ] protected Transform swap_point_in;
+    [ BoxGroup( "Setup" ), SerializeField ] protected Transform swap_point_out;
 #endregion
 
 #region Properties
@@ -33,7 +34,7 @@ public abstract class SwapTriggerLane : MonoBehaviour
 #endregion
 
 #region Implementation
-	protected abstract void SwapPlayerIn( Collider player );
+	protected abstract void SwapPlayerIn( Collider collider );
 
     protected virtual float SwapPlayerOut()
     {
@@ -44,6 +45,21 @@ public abstract class SwapTriggerLane : MonoBehaviour
 
 #region Editor Only
 #if UNITY_EDITOR
+	private void OnDrawGizmosSelected()
+	{
+		var position = swap_point_out.position;
+		var position_left = swap_point_out.position - Vector3.left * GameSettings.Instance.swap_point_out_randomness;
+		var position_right = swap_point_out.position - Vector3.right * GameSettings.Instance.swap_point_out_randomness;
+
+		Handles.color = Color.blue;
+		Handles.DrawDottedLine( position_left, position_right, 1f );
+		Handles.Label( position + Vector3.up, "Swap Point Out" );
+		Handles.DrawSolidDisc( position, Vector3.up, 1 );
+
+		Handles.color = Color.yellow;
+		Handles.DrawSolidDisc( position_left, Vector3.up, 1 );
+		Handles.DrawSolidDisc( position_right, Vector3.up, 1 );
+	}
 #endif
 #endregion
 }
