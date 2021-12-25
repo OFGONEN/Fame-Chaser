00bfc05ff49d3d251085895f2b223682203daa46
ofgonen <ofgonen@gmail.com>
2021-12-25 14:10:03 +0300

    Implement SwapTriggerLane
    - Abstract

---
+++ Assets/Script/SwapTriggerLane.cs
@@ -0,0 +1,44 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using NaughtyAttributes;
+
+public abstract class SwapTriggerLane : MonoBehaviour
+{
+#region Fields
+    [ BoxGroup( "Setup" ), SerializeField ] private TriggerListener_Enter swap_trigger;
+    [ BoxGroup( "Setup" ), SerializeField ] private Transform swap_point_int;
+    [ BoxGroup( "Setup" ), SerializeField ] private Transform swap_point_out;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    private void OnEnable()
+    {
+		swap_trigger.Subscribe( SwapPlayerIn );
+	}
+
+    private void OnDisable()
+    {
+		swap_trigger.Unsubscribe( SwapPlayerIn );
+    }
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+	protected abstract void SwapPlayerIn( Collider player );
+    protected abstract Vector3 SwapPlayerOut();
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
