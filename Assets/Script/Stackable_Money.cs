d85d41d79b7d582e44a1860cceef5e84c0e6dfdc
ofgonen <ofgonen@gmail.com>
2021-12-27 21:36:51 +0300

    Implement Stackable_Money

---
+++ Assets/Script/Stackable_Money.cs
@@ -0,0 +1,45 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+public class Stackable_Money : MonoBehaviour
+{
+#region Fields
+    [ SerializeField ] private Stackable_Money_Pool money_pool;
+    [ SerializeField ] private ParticleSpawnEvent money_particle;
+    public int money_count;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void Deposit() 
+    {
+		gameObject.SetActive( false );
+		money_pool.ReturnEntity( this );
+	}
+
+    public void DepositWithMoney() 
+    {
+		money_particle.Raise( "stack_money", transform.position );
+
+		gameObject.SetActive( false );
+		money_pool.ReturnEntity( this );
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
